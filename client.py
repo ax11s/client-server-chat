c253cec695d916c2754e93031e00dbe4a51960b0
ax11 <125132362+ax11s@users.noreply.github.com>
2023-03-07 16:05:52 +0100

    Rewrite

--- client.py
+++ client.py
@@ -1,16 +1,42 @@
 import socket
+import threading
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 12345
 
-nickname = input("Enter your nickname: ")
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    print("connected succesfuly")
-    while True:
-        message = input("::")
-        full_message = f"{nickname}: {message}"
-        s.sendall(full_message.encode())
-        data = s.recv(1024)
-        print(data.decode())
+
+ip = input('Podaj adresik serwera: ')
+uname = input('Podaj nick: ')
+try:
+    s.connect((ip, port))
+except:
+    print("\n\t ZLY ADRES AASDDAASDA\n")
+try:
+    s.send(uname.encode('UTF-8'))
+
+
+    running = True
+
+    def receiveMsg(sock):
+        serverdead = False
+        while running and (not serverdead):
+            try:
+                msg = sock.recv(1024).decode('UTF-8')
+                print(msg)
+            except:
+                print('W SERWEJ JEBNEL PIERUN \n kliknij cos zeby zamknac kienta')
+                serverdead = True
+
+    threading.Thread(target = receiveMsg, args = (s,)).start()
+    while running:
+        tempMsg = input()
+        msg = uname + ' >> ' + tempMsg
+        if '/exit' in msg:
+            running = False
+            s.send('/exit'.encode('UTF-8'))
+        else:
+            s.send(msg.encode('UTF-8'))
+
+except:
+    print("\n\t Kurwa nwm error te\n")
--- server.py
+++ server.py
@@ -1,21 +1,87 @@
 import socket
+import threading
+import datetime
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print("Server listening on", (HOST, PORT))
-    conn, addr = s.accept()
-    print('Connected by', addr)
-    with conn:
-        while True:
-            s.listen()
-            conn, addr = s.accept()
-            print('Connected by', addr)
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(data.decode())
-            conn.sendall(data)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverRunning = True
+ip = str(socket.gethostbyname(socket.gethostname()))
+port = 12345
+adress = '127.0.0.1'
+
+clients = {}
+
+s.bind((adress, port))
+s.listen()
+print('Starcik serwerka :)')
+print('IP: ' + ip)
+
+
+def handleClient(client, uname):
+    clientConnected = True
+    keys = clients.keys()
+    help = '/all - to do wszystich piszesz\n /komendy - to masz to menu \n /exit - wydupiasz z serwera\n /ludziki - daje ci liste ludzikow na serwerze \n /msg - to piszesz do kogos bezposrednio np. /msg debil \n'
+
+    while clientConnected:
+        try:
+            
+            msg = client.recv(1024).decode('UTF-8')
+            response = 'Ludziki na serwerku :\n'
+            found = False
+            
+            if '/ludziki' in msg:
+
+                clientNo = 0
+
+                for name in keys:
+                    clientNo += 1
+                    response = response + str(clientNo) +'\t:' + name+'\n'
+                client.send(response.encode('UTF-8'))
+
+            elif '/komendy' in msg:
+                client.send(help.encode('UTF-8'))
+
+            elif '/all' in msg:
+                msg = msg.replace('/all','')
+                for k,v in clients.items():
+                    v.send(msg.encode('UTF-8'))
+                
+            elif '/exit' in msg:
+                response = 'Cwel sie rozlaczyl'
+                client.send(response.encode('UTF-8'))
+                clients.pop(uname)
+                print(uname + ' LEFT')
+                clientConnected = False
+
+            else:
+
+                for name in keys:
+                    if('/msg '+name) in msg:
+                        msg = msg.replace('@'+name, '')
+                        clients.get(name).send(msg.encode('UTF-8'))
+                        found = True
+
+                if(not found):
+                    client.send('Zly nick debilu'.encode('UTF-8'))
+
+        except:
+            clients.pop(uname)
+            print(uname + ' LEFT')
+            clientConnected = False
+
+
+        
+
+
+while serverRunning:
+    client, address = s.accept()
+    uname = client.recv(1024).decode('ascii')
+    print('Polaczono z serwerkiem'%str(uname))
+    print('------------------------------------------------------------------------------------------------------')
+    client.send('Elo, tu masz komendy: /komendy'.encode('ascii'))
+    
+    if(client not in clients):
+        clients[uname] = client
+        threading.Thread(target = handleClient, args = (client, uname, )).start()
+        
